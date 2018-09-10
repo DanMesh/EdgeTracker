9c92a6053b22e83d828fdb7f6fde69cfae045657
Daniel Mesham <danielmesham@gmail.com>
2018-09-10 11:24:31 +0200

    Allowed for multiple object tracking

--- EdgeTracker/main.cpp
+++ EdgeTracker/main.cpp
@@ -57,11 +57,13 @@ int main(int argc, const char * argv[]) {
     //   MODEL CREATION
     // * * * * * * * * * * * * * * * * *
     
-    //Model * model = new Rectangle(60, 80, Scalar(20, 65, 165));
-    Model * model = new Dog(Scalar(19, 89, 64));
-    //Model * model = new Arrow(Scalar(108, 79, 28));
+    Model * modelRect = new Rectangle(60, 80, Scalar(20, 65, 165));
+    Model * modelDog = new Dog(Scalar(19, 89, 64));
+    Model * modelArrow = new Arrow(Scalar(108, 79, 28));
     //Model * model = new Box(175, 210, 49, Scalar(8, 152, 206)); // Yellow box
     //Model * model = new Box(204, 257, 70, Scalar(71, 92, 121)); // Brown box
+    
+    vector<Model *> model = {modelRect, modelDog, modelArrow};
     
     // * * * * * * * * * * * * * * * * *
     //   OPEN THE FIRST FRAME
@@ -76,52 +78,62 @@ int main(int argc, const char * argv[]) {
     imshow("Frame", frame);
     
     // * * * * * * * * * * * * * * * * *
-    //   LOCATE THE STARTING POSITION
+    //   LOCATE THE STARTING POSITIONS
     // * * * * * * * * * * * * * * * * *
-    //      Assume the object is
+    //      Assume the objects are
     //      leaning back at
     //      ±45 degrees
     // * * * * * * * * * * * * * * * * *
     
-    // Find the area & centoid of the object in the image
-    Mat segInit = orange::segmentByColour(frame, model->colour);
-    cvtColor(segInit, segInit, CV_BGR2GRAY);
-    threshold(segInit, segInit, 0, 255, CV_THRESH_BINARY);
-    Point centroid = ASM::getCentroid(segInit);
-    double area = ASM::getArea(segInit);
+    vector<estimate> est;
     
-    // Draw the model at the default position and find the area & cetroid
-    Vec6f pose = {0, 0, 300, -CV_PI/4, 0, 0};
-    Mat initGuess = Mat::zeros(frame.rows, frame.cols, frame.type());
-    model->draw(initGuess, pose, K);
-    cvtColor(initGuess, initGuess, CV_BGR2GRAY);
-    threshold(initGuess, initGuess, 0, 255, CV_THRESH_BINARY);
-    Point modelCentroid = ASM::getCentroid(initGuess);
-    double modelArea = ASM::getArea(initGuess);
-    
-    // Convert centroids to 3D/homogeneous coordinates
-    Mat centroid2D;
-    hconcat( Mat(centroid), Mat(modelCentroid), centroid2D );
-    vconcat(centroid2D, Mat::ones(1, 2, centroid2D.type()), centroid2D);
-    centroid2D.convertTo(centroid2D, K.type());
-    Mat centroid3D = K.inv() * centroid2D;
-    
-    // Estimate the depth from the ratio of the model and measured areas,
-    // and create a pose guess from that.
-    // Note that the x & y coordinates need to be calculated using the pose
-    // of the centroid relative to the synthetic model image's centroid.
-    double zGuess = pose[2] * sqrt(modelArea/area);
-    centroid3D *= zGuess;
-    pose[0] = centroid3D.at<float>(0, 0) - centroid3D.at<float>(0, 1);
-    pose[1] = centroid3D.at<float>(1, 0) - centroid3D.at<float>(1, 1);
-    pose[2] = zGuess;
-    waitKey(0);
-    
-    // Set the intial pose
-    estimate est = estimate(pose, 0, 0);
+    // Find the initial pose of each model
+    for (int m = 0; m < model.size(); m++) {
+        
+        // Find the area & centoid of the object in the image
+        Mat segInit = orange::segmentByColour(frame, model[m]->colour);
+        cvtColor(segInit, segInit, CV_BGR2GRAY);
+        threshold(segInit, segInit, 0, 255, CV_THRESH_BINARY);
+        Point centroid = ASM::getCentroid(segInit);
+        double area = ASM::getArea(segInit);
+        
+        // Draw the model at the default position and find the area & cetroid
+        Vec6f initPose = {0, 0, 300, -CV_PI/4, 0, 0};
+        Mat initGuess = Mat::zeros(frame.rows, frame.cols, frame.type());
+        model[m]->draw(initGuess, initPose, K);
+        cvtColor(initGuess, initGuess, CV_BGR2GRAY);
+        threshold(initGuess, initGuess, 0, 255, CV_THRESH_BINARY);
+        Point modelCentroid = ASM::getCentroid(initGuess);
+        double modelArea = ASM::getArea(initGuess);
+        
+        // Convert centroids to 3D/homogeneous coordinates
+        Mat centroid2D;
+        hconcat( Mat(centroid), Mat(modelCentroid), centroid2D );
+        vconcat(centroid2D, Mat::ones(1, 2, centroid2D.type()), centroid2D);
+        centroid2D.convertTo(centroid2D, K.type());
+        Mat centroid3D = K.inv() * centroid2D;
+        
+        // Estimate the depth from the ratio of the model and measured areas,
+        // and create a pose guess from that.
+        // Note that the x & y coordinates need to be calculated using the pose
+        // of the centroid relative to the synthetic model image's centroid.
+        double zGuess = initPose[2] * sqrt(modelArea/area);
+        centroid3D *= zGuess;
+        initPose[0] = centroid3D.at<float>(0, 0) - centroid3D.at<float>(0, 1);
+        initPose[1] = centroid3D.at<float>(1, 0) - centroid3D.at<float>(1, 1);
+        initPose[2] = zGuess;
+        
+        // Set the intial pose
+        estimate initEst = estimate(initPose, 0, 0);
+        
+        // Add the estimate to the list
+        est.push_back(initEst);
+    }
     
     // Pause to allow annotation
-    // addMouseHandler("Frame"); waitKey(0);
+    // addMouseHandler("Frame");
+    
+    waitKey(0);
     
     // * * * * * * * * * * * * * * * * *
     //   CAMERA LOOP
@@ -133,59 +145,64 @@ int main(int argc, const char * argv[]) {
     while (!frame.empty()) {
         
         auto start = chrono::system_clock::now();   // Start the timer
-                
-        // Segment by colour
-        Mat seg = orange::segmentByColour(frame, model->colour);
         
-        // Detect edges
-        Mat canny;
-        Canny(seg, canny, 70, 210);
-        
-        // Extract the image edge point coordinates
-        Mat edges;
-        findNonZero(canny, edges);
-        
-        int iterations = 1;
-        double error = lsq::ERROR_THRESHOLD + 1;
-        while (error > lsq::ERROR_THRESHOLD && iterations < 20) {
-            // Generate a set of whiskers
-            vector<Whisker> whiskers = ASM::projectToWhiskers(model, est.pose, K);
+        // Find the pose of each model
+        for (int m = 0; m < model.size(); m++) {
             
-            //Mat cannyTest;
-            //canny.copyTo(cannyTest);
+            // Segment by colour
+            Mat seg = orange::segmentByColour(frame, model[m]->colour);
             
-            // Sample along the model edges and find the edges that intersect each whisker
-            Mat targetPoints = Mat(2, 0, CV_32S);
-            Mat whiskerModel = Mat(4, 0, CV_32FC1);
-            for (int w = 0; w < whiskers.size(); w++) {
-                Point closestEdge = whiskers[w].closestEdgePoint(edges);
-                if (closestEdge == Point(-1,-1)) continue;
-                hconcat(whiskerModel, whiskers[w].modelCentre, whiskerModel);
-                hconcat(targetPoints, Mat(closestEdge), targetPoints);
+            // Detect edges
+            Mat canny;
+            Canny(seg, canny, 70, 210);
+            
+            // Extract the image edge point coordinates
+            Mat edges;
+            findNonZero(canny, edges);
+            
+            int iterations = 1;
+            double error = lsq::ERROR_THRESHOLD + 1;
+            while (error > lsq::ERROR_THRESHOLD && iterations < 20) {
+                // Generate a set of whiskers
+                vector<Whisker> whiskers = ASM::projectToWhiskers(model[m], est[m].pose, K);
                 
-                //TRACE: Display the whiskers
-                //circle(cannyTest, closestEdge, 3, Scalar(120));
-                //circle(cannyTest, whiskers[w].centre, 3, Scalar(255));
-                //line(cannyTest, closestEdge, whiskers[w].centre, Scalar(150));
+                //Mat cannyTest;
+                //canny.copyTo(cannyTest);
+                
+                // Sample along the model edges and find the edges that intersect each whisker
+                Mat targetPoints = Mat(2, 0, CV_32S);
+                Mat whiskerModel = Mat(4, 0, CV_32FC1);
+                for (int w = 0; w < whiskers.size(); w++) {
+                    Point closestEdge = whiskers[w].closestEdgePoint(edges);
+                    if (closestEdge == Point(-1,-1)) continue;
+                    hconcat(whiskerModel, whiskers[w].modelCentre, whiskerModel);
+                    hconcat(targetPoints, Mat(closestEdge), targetPoints);
+                    
+                    //TRACE: Display the whiskers
+                    //circle(cannyTest, closestEdge, 3, Scalar(120));
+                    //circle(cannyTest, whiskers[w].centre, 3, Scalar(255));
+                    //line(cannyTest, closestEdge, whiskers[w].centre, Scalar(150));
+                }
+                //imshow("CannyTest", cannyTest);
+                
+                targetPoints.convertTo(targetPoints, CV_32FC1);
+                
+                // Use least squares to match the sampled edges to each other
+                est[m] = lsq::poseEstimateLM(est[m].pose, whiskerModel, targetPoints.t(), K, 2);
+                
+                
+                double improvement = (error - est[m].error)/error;
+                error = est[m].error;
+                
+                // Stop trying if you reduce the error by < 1% (excl. the first one)
+                if (improvement < 0.01 && iterations > 1) break;
+                
+                iterations++;
+                //waitKey(0);
             }
-            //imshow("CannyTest", cannyTest);
+            cout << "Iterations = " << iterations << endl;
             
-            targetPoints.convertTo(targetPoints, CV_32FC1);
-            
-            // Use least squares to match the sampled edges to each other
-            est = lsq::poseEstimateLM(est.pose, whiskerModel, targetPoints.t(), K, 2);
-            
-            
-            double improvement = (error - est.error)/error;
-            error = est.error;
-            
-            // Stop trying if you reduce the error by < 1% (excl. the first one)
-            if (improvement < 0.01 && iterations > 1) break;
-            
-            iterations++;
-            //waitKey(0);
         }
-        cout << "Iterations = " << iterations << endl;
         
         // Stop timer and show time
         auto stop = chrono::system_clock::now();
@@ -195,8 +212,10 @@ int main(int argc, const char * argv[]) {
         times.push_back(time);
         if (time > longestTime) longestTime = time;
         
-        // Draw the shape on the image
-        model->draw(frame, est.pose, K, model->colour);
+        // Draw the shapes on the image
+        for (int m = 0; m < model.size(); m++) {
+            model[m]->draw(frame, est[m].pose, K, model[m]->colour);
+        }
         imshow("Frame", frame);
         
         // Get next frame
